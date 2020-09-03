4397ae7060251ba7133ad6e66660d3f9e417f266
irina844 <i123469@yandex.ru>
2020-09-03 17:42:23 +0300

    HW1
     Созданы переменные всех пройденных типов данных, и инициализированы их значения;

--- src/main/java/main/main.java
+++ src/main/java/main/main.java
@@ -2,6 +2,14 @@ package main;
 
 public class main {
     public static void main(){
-        
+        getByte = 0;
+        putByte = 0;
+        short a = 0;
+        int b = 0;
+        long c = 0;
+        float d = 31.31f;
+        double e = 3.12;
+        char f = 'f';
+        string h = "asd";
     }
 }
