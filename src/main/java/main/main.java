0d0a4337490b9374af13adbfb7003977167abca4
irina844 <i123469@yandex.ru>
2020-09-03 17:53:04 +0300

    HW1
    Написан метод вычисляющий выражение a * (b + (c / d)) и возвращающий результат,где a, b, c, d – входные параметры этого метода;

--- src/main/java/main/main.java
+++ src/main/java/main/main.java
@@ -1,7 +1,8 @@
 package main;
+import java.util.Scanner;
 
 public class main {
-    public static void main(){
+    public static void main(String[]args){
         getByte = 0;
         putByte = 0;
         short a = 0;
@@ -11,5 +12,15 @@ public class main {
         double e = 3.12;
         char f = 'f';
         string h = "asd";
+        check();
+    }
+    public static int check(){
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        int d = in.nextInt();
+        System.out.println(a *(b+(c/d)));
+        return 0;
     }
 }
